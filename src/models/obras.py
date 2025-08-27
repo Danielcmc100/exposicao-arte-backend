b5ca09de43f89c4777701ff967c127af4957a14d
danielcmc100 <danielcmc100@gmail.com>
2025-08-26 22:51:02 -0300

    refactor: reorganiza importações e campo tags como json

--- src/models/obras.py
+++ src/models/obras.py
@@ -1,37 +1,26 @@
 from datetime import datetime
-from sqlmodel import Field, SQLModel
-from sqlalchemy.sql import func
+
+from sqlalchemy import JSON
+from sqlmodel import Field, SQLModel, func
+
 
 class ObraBase(SQLModel):
     titulo: str
     autor: str
-    ano_producao: datetime  
+    ano_producao: datetime
     tecnica_criacao: str
     altura_centimetros: float
     largura_centimetros: float
-    comprimento_centimetros: float
     peso_quilos: float
-    tags: list[str]
+    tags: list[str] = Field(sa_type=JSON)
+    preco: float
     preco: float
     status: bool
 
+
 class ObraDB(ObraBase, table=True):
-    __tablename__ = "obras" # type: ignore
+    __tablename__ = "obras"  # type: ignore
     id: int = Field(default=None, primary_key=True)
-    data_postagem: datetime = Field (default = func.now())
+    data_postagem: datetime = Field(default=func.now())
     id_artista: int
     id_categoria: int
-   
-
-    
-
-
-
-
-
-
-
-
-
-
-    
