6beb128648c8f5314e76b9ce8a216af0fb14be2c
Hugo Norte <hugonorte@gmail.com>
2025-08-30 18:21:19 -0300

    feat: implementa CRUD para eventos com rotas e repositório

---
+++ src/routers/evento.py
@@ -0,0 +1,55 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlmodel import Session
+
+from database import get_session
+
+from models.evento import EventoCreate, EventoDB, EventoResponse
+
+from repositories.evento import (
+    adicionar_evento,
+    atualizar_evento,
+    buscar_evento_por_id,
+    buscar_eventos,
+    remover_evento,
+)
+
+rota = APIRouter(prefix="/eventos", tags=["eventos"])
+
+SessionInjetada = Annotated[Session, Depends(get_session)]
+
+
+@rota.get("/")
+def obter_eventos(session: SessionInjetada) -> list[EventoResponse]:
+    eventos_list = buscar_eventos(session)
+    return list(map(EventoResponse.model_validate, eventos_list))
+
+
+@rota.get("/{evento_id}")
+def ler_evento(evento_id: int, session: SessionInjetada) -> EventoResponse | None:
+    evento = buscar_evento_por_id(evento_id, session)
+    return EventoResponse.model_validate(evento) if evento else None
+
+
+@rota.post("/")
+def criar_evento(evento: EventoCreate, session: SessionInjetada) -> EventoResponse:
+    evento_db = EventoDB.model_validate(evento)
+    return EventoResponse.model_validate(adicionar_evento(evento_db, session))
+
+
+@rota.put("/{evento_id}")
+def atualizar_evento(
+    evento_id: int, evento: EventoCreate, session: SessionInjetada
+) -> EventoResponse | None:
+    evento_db = EventoDB.model_validate(evento)
+    evento_atualizado = atualizar_evento(evento_id, evento_db, session)
+    return EventoResponse.model_validate(evento_atualizado)
+
+
+@rota.delete("/{evento_id}")
+def excluir_evento(
+    evento_id: int, session: SessionInjetada
+) -> EventoResponse | None:
+    evento_removido = remover_evento(evento_id, session)
+    return EventoResponse.model_validate(evento_removido)
