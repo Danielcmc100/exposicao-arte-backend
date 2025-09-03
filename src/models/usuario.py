314d66c7d69ef058cc7ee9db6ad7f189339ba206
Hugo Norte <hugonorte@gmail.com>
2025-09-03 18:59:33 -0300

    feat: adiciona relacionamento com LinkRedeDB na classe UsuarioDB

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -1,6 +1,8 @@
 from enum import Enum, auto
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
+from .link_rede import LinkRedeDB
+from typing import List, Optional
 
 class Funcao(Enum):
     CONSUMIDOR = auto()
@@ -26,3 +28,5 @@ class UsuarioDB(UsuarioCreate, table=True):
     __tablename__ = "usuarios"  # type: ignore
 
     id: int = Field(default=None, primary_key=True)
+
+    links: List["LinkRedeDB"] = Relationship(back_populates="usuario")
