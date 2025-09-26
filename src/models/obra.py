c53beffabeb9ac568420bcd04724f88c791b1dbc
Hugo Norte <hugonorte@gmail.com>
2025-09-26 20:35:11 -0300

    feat: implement CRUD operations for obras and update related models and routes

--- src/models/obra.py
+++ src/models/obra.py
@@ -1,7 +1,13 @@
+from typing import TYPE_CHECKING, Optional
+
 from datetime import datetime
 
 from sqlalchemy import JSON
-from sqlmodel import Field, SQLModel, func
+from sqlmodel import Field, SQLModel, func, Relationship
+
+if TYPE_CHECKING:
+    from .usuario import UsuarioDB
+    from .categoria import CategoriaDB
 
 
 class ObraBase(SQLModel):
@@ -14,13 +20,24 @@ class ObraBase(SQLModel):
     peso_quilos: float
     tags: list[str] = Field(sa_type=JSON)
     preco: float
-    preco: float
     status: bool
-
-
-class ObraDB(ObraBase, table=True):
-    __tablename__ = "obra"  # type: ignore
-    id: int = Field(default=None, primary_key=True)
     data_postagem: datetime = Field(default=func.now())
     id_artista: int
     id_categoria: int
+
+class ObraCreate(ObraBase):
+    pass
+
+
+class ObraResponse(ObraBase):
+    id: int
+
+class ObraDB(ObraCreate, table=True):
+    __tablename__ = "obras"  # type: ignore
+    id: int = Field(default=None, primary_key=True)
+
+    usuario_id: int = Field(foreign_key="usuarios.id", nullable=False)
+    usuario: "UsuarioDB" = Relationship(back_populates="obras")
+
+    categoria_id: int = Field(foreign_key="categorias.id", nullable=False)
+    categoria: "CategoriaDB" = Relationship(back_populates="obras")
