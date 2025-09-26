4c282bd228f8bb59506e382c5d4dd22b4a03fb25
danielcmc100 <danielcmc100@gmail.com>
2025-09-26 18:48:42 -0300

    refactor: clean up imports in usuario.py

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -2,17 +2,11 @@ from enum import Enum, auto
 from typing import TYPE_CHECKING, List
 
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel, Relationship
-from typing import List, Optional, TYPE_CHECKING
-
 from sqlmodel import Field, Relationship, SQLModel
 
 if TYPE_CHECKING:
     from .comentario_evento import ComentarioEventoDB
-
     from .link_rede import LinkRedeDB
-
-if TYPE_CHECKING:
 
 
 class Funcao(Enum):
